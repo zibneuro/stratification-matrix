cdc46680c96b3ef725bddcc8b22cbc6c1bb4ab85
Philipp Harth <harth@zib.de>
2022-06-21 23:25:27 +0200

    aggregate data

--- server/preprocess_COVID.py
+++ server/preprocess_COVID.py
@@ -5,41 +5,107 @@ import numpy as np
 
 import util 
 
+
 def printUsageAndExit():
     print("python preprocess_COVID.py data-folder")
     exit()
-    
-
-def getNeuronProperties(filename, propertyName):    
-    propertiesFile = os.path.join(filename)
-    with open(propertiesFile) as f:
-        properties = json.load(f)
-    propertyIdx = {}
-    for i in range(0, len(properties["inline"]["properties"])):
-        propName = properties["inline"]["properties"][i]["id"]
-        #print(i, propName)
-        propertyIdx[propName] = i
-
-    tags_all = properties["inline"]["properties"][propertyIdx["tags"]]["tags"]    
-    values_all = properties["inline"]["properties"][propertyIdx[propertyName]]["values"]    
-    ids_all = properties["inline"]["ids"]
-        
-    return ids_all, tags_all, values_all
 
 
-def splitTagDataColumns(dataColumn):
-    n = len(dataColumn)    
-    col1 = np.zeros(n, dtype=int)
-    col2 = np.zeros(n, dtype=int)
-    col3 = np.zeros(n, dtype=int)
-    for i in range(0,n):
-        col1[i] = dataColumn[i][0]
-        col2[i] = dataColumn[i][1]
-        #col3[i] = dataColumn[i][2]
-    return col1, col2, col3
+def getDateIndex():
+    return {
+        "2020-01" : 0,
+        "2020-02" : 1,
+        "2020-03" : 2,
+        "2020-04" : 3,
+        "2020-05" : 4,
+        "2020-06" : 5,
+        "2020-07" : 6,
+        "2020-08" : 7,
+        "2020-09" : 8,
+        "2020-10" : 9,
+        "2020-11" : 10,
+        "2020-12" : 11,
+        "2021-01" : 12,
+        "2021-02" : 13,
+        "2021-03" : 14,
+        "2021-04" : 15,
+        "2021-05" : 16,
+        "2021-06" : 17,
+        "2021-07" : 18,
+        "2021-08" : 19,
+        "2021-09" : 20,
+        "2021-10" : 21,
+        "2021-11" : 22,
+        "2021-12" : 23,
+        "2022-01" : 24,
+        "2022-02" : 25,
+        "2022-03" : 26,
+        "2022-04" : 27,
+        "2022-05" : 28,
+        "2022-06" : 29
+    }
 
 
-def printValueRange(filename):
+def getAgeIndex():
+    return {
+        'A00-A04' : 0, 
+        'A05-A14' : 1,
+        'A15-A34' : 2, 
+        'A35-A59' : 3,         
+        'A60-A79' : 4,         
+        'A80+' : 5,
+        'na' : 6
+    }
+
+
+def getGenderIndex():
+    return {
+        "M" : 0,
+        "W" : 1,
+        "na" : 2
+    }
+
+def getLandIndex():
+    """
+    - 01 Schleswig-Holstein (SH)
+    - 02 Hamburg (HH)
+    - 03 Niedersachsen (NI)
+    - 04 Bremen (HB)
+    - 05 Nordrhein-Westfalen (NW)
+    - 06 Hessen (HE)
+    - 07 Rheinland-Pfalz (RP)
+    - 08 Baden-Württemberg (BW)
+    - 09 Bayern (BY)
+    - 10 Saarland (SL)
+    - 11 Berlin (BE)
+    - 12 Brandenburg (BB)
+    - 13 Mecklenburg-Vorpommern (MV)
+    - 14 Sachsen (SN)
+    - 15 Sachsen-Anhalt (ST)
+    - 16 Thüringen (TH)
+    """
+    return {
+        "Schleswig-Holstein" : 1,
+        "Hamburg" : 2,
+        "Niedersachsen" : 3,
+        "Bremen" : 4,
+        "Nordrhein-Westfalen" : 5,
+        "Hessen" : 6,
+        "Rheinland-Pfalz" : 7,
+        "Baden-Wuerttemberg" : 8,
+        "Bayern" : 9,
+        "Saarland" : 10,
+        "Berlin" : 11,
+        "Brandenburg" : 12,
+        "Mecklenburg-Vorpommern" : 13,
+        "Sachsen" : 14,
+        "Sachsen-Anhalt" : 15,
+        "Thueringen" : 16
+    }
+
+
+
+def aggregateData(filename, filenameAggregated):
     with open(filename, encoding='utf-8-sig') as f:
         lines = f.readlines()
         headers = lines[0].rstrip().split(",")
@@ -51,7 +117,7 @@ def printValueRange(filename):
         allGenders = set()
         sumCases = 0
         sumDeaths = 0
-
+               
         def getColIdx(colName):
             return headers.index(colName)
 
@@ -59,7 +125,7 @@ def printValueRange(filename):
             return dateString[0:7]
 
         def getLand(landkreisId):
-            return landkreisId[:-3]
+            return int(landkreisId[:-3])
 
         for i in range(1,len(lines)):
             parts = lines[i].rstrip().split(",")
@@ -75,28 +141,61 @@ def printValueRange(filename):
 
             gender = parts[getColIdx("Geschlecht")]
             allGenders.add(gender)
-
-            neuerFall = int(parts[getColIdx("NeuerFall")])
-            #if(neuerFall in [0,1,-1]):
+           
             anzahlFall = int(parts[getColIdx("AnzahlFall")])                
             sumCases += anzahlFall
 
-            neuerTodesfall = int(parts[getColIdx("NeuerTodesfall")])
-            #if(neuerTodesfall in [1,-1]):
             anzahlTodesfall = int(parts[getColIdx("AnzahlTodesfall")])                
             sumDeaths += anzahlTodesfall
 
-            
         allYearMonth = list(allYearMonth)
         allYearMonth.sort()
         for i in range(0,len(allYearMonth)):
-            print(i,allYearMonth[i])       
+            print(allYearMonth[i], i)       
 
         print(allLand)
         print(allAgeGroups)
         print(allGenders)
         print("cases", sumCases)
         print("deaths", sumDeaths)
+
+    	# aggregate 
+        dateIndex = getDateIndex()
+        ageIndex = getAgeIndex()
+        genderIndex = getGenderIndex()
+
+        cases = {} # (date, age, gender, land) => [cases, deaths]
+
+        for i in range(1,len(lines)):
+            parts = lines[i].rstrip().split(",")
+
+            dateString = parts[getColIdx("Meldedatum")]            
+            dateId = dateIndex[getYearMonth(dateString)]
+
+            landkreisId = parts[getColIdx("IdLandkreis")]
+            landId = getLand(landkreisId)
+
+            ageGroup = parts[getColIdx("Altersgruppe")].replace("unbekannt","na")
+            ageId = ageIndex[ageGroup]
+
+            gender = parts[getColIdx("Geschlecht")].replace("unbekannt","na")
+            genderId = genderIndex[gender]
+           
+            anzahlFall = int(parts[getColIdx("AnzahlFall")])                        
+            anzahlTodesfall = int(parts[getColIdx("AnzahlTodesfall")])      
+
+            caseKey = (dateId, landId, ageId, genderId)          
+            if(caseKey not in cases):
+                cases[caseKey] = [0, 0]
+
+            cases[caseKey][0] += anzahlFall
+            cases[caseKey][1] += anzahlTodesfall
+
+        with open(filenameAggregated, "w") as f:
+            f.write("date_id,age_id,gender_id,land_id,cases,deaths\n")
+            for caseKey, values in cases.items():
+                f.write("{},{},{},{},{},{}\n".format(*caseKey, *values))
+    
 
 
 if __name__ == "__main__":
@@ -114,8 +213,9 @@ if __name__ == "__main__":
     samplesFile0 = os.path.join(outfolder_channel0, "samples0")
     samplesFile1 = os.path.join(outfolder_channel0, "samples1")
 
-    dataFile = os.path.join(dataFolder, "COVID", "Aktuell_Deutschland_SarsCov2_Infektionen.csv")
-    printValueRange(dataFile)
+    dataFile = os.path.join(dataFolder, "COVID", "Aktuell_Deutschland_SarsCov2_Infektionen.csv") # downloaded 2022-06-21
+    dataFileAggregated = os.path.join(dataFolder, "COVID", "aggregated.csv")
+    #aggregateData(dataFile, dataFileAggregated)
     
     """
     ids, _, dataColumn = getNeuronProperties(propertiesFile, "tags")
